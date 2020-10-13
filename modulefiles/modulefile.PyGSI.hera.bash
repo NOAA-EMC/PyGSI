6f398ca9f1375a30fddd363ef0269625e28e5c85
Kevin Dougherty <kevin.dougherty@noaa.gov>
2020-10-13 14:37:48 +0000

    addressed conda init not working

--- modulefiles/modulefile.PyGSI.hera.bash
+++ modulefiles/modulefile.PyGSI.hera.bash
@@ -1,5 +1,7 @@
 export PATH="/scratch1/NCEPDEV/da/Kevin.Dougherty/anaconda3/bin:$PATH"
 export PYTHONPATH="${PYTHONPATH}:/home/Kevin.Dougherty/PyGSI/"
 
+source /scratch1/NCEPDEV/da/Kevin.Dougherty/anaconda3/etc/profile.d/conda.sh
+
 conda init bash
 conda activate PyGSI
