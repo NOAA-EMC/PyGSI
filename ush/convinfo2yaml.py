2d0a406ea461358fde20068b55a72c64d4aa8448
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2021-01-04 20:04:01 +0000

    Update convinfo2yaml for pep8

--- ush/convinfo2yaml.py
+++ ush/convinfo2yaml.py
@@ -7,14 +7,15 @@ import yaml
 import glob
 import csv
 
+
 def main(config):
     # read in convinfo file
-    obtype = [] # string used in the filename
-    typeint = [] # integer value of observation type
-    subtypeint = [] # integer value of observation subtype
-    obuse = [] # use 1, monitor -1
+    obtype = []  # string used in the filename
+    typeint = []  # integer value of observation type
+    subtypeint = []  # integer value of observation subtype
+    obuse = []  # use 1, monitor -1
     cv = open(config['convinfo'])
-    rdcv = csv.reader(filter(lambda row: row[0]!='!', cv))
+    rdcv = csv.reader(filter(lambda row: row[0] != '!', cv))
     for row in rdcv:
         try:
             rowsplit = row[0].split()
@@ -23,29 +24,30 @@ def main(config):
             subtypeint.append(rowsplit[2])
             obuse.append(rowsplit[3])
         except IndexError:
-            pass # end of file
+            pass  # end of file
     cv.close()
 
     # get list of conventional diagnostic files available
-    diagpath = config['diagdir'] + '/diag_conv_*_' + config['loop'] + '.' + config['cycle'] + '.nc4'
+    diagpath = '%s/diag_conv_*_%s.%s.nc4' % (config['diagdir'],
+                                             config['loop'], config['cycle'])
     diagfiles = glob.glob(diagpath)
 
     # initialize YAML dictionary for output
-    yamlout = { 'diagnostic': [] }
-    if config['loop'] == 'ges':
-        diagtype = 'O-F'
-    else:
-        diagtype = 'O-A'
+    yamlout = {'diagnostic': []}
+    diagtype = 'O-A' if config['loop'] == 'anl' else 'O-F'
     figs = ['histogram', 'spatial']
 
     # loop through obtypes
     for i in range(len(obtype)):
         # first get filename and verify it exists
-        diagfile = config['diagdir'].rstrip('/') + '/diag_conv_' + obtype[i] + '_' + config['loop'] + '.' + config['cycle'] + '.nc4'
+        diagfile = '%s/%s_%s_%s.%s.nc4' % ('diag_conv',
+                                           config['diagdir'].rstrip('/'),
+                                           obtype[i], config['loop'],
+                                           config['cycle'])
         if diagfile not in diagfiles:
-            continue # skip if diag file is missing
+            continue  # skip if diag file is missing
         if int(obuse[i]) != 1:
-            continue # only process assimilated obs for now
+            continue  # only process assimilated obs for now
         dictloop = {
                    'path': [diagfile],
                    'observation id': [int(typeint[i])],
@@ -61,13 +63,22 @@ def main(config):
         yaml.dump(yamlout, file, default_flow_style=False)
     print('YAML written to '+config['yaml'])
 
-parser = argparse.ArgumentParser(description='Given an input convinfo GSI file and path to GSI diags,'+\
-                                ' generate an output YAML file for use by PyGSI')
-parser.add_argument('-d','--diagdir', type=str, help='path to GSI netCDF diags', required=True)
-parser.add_argument('-c','--cycle', type=str, help='cycle YYYYMMDDHH', required=True)
-parser.add_argument('-i','--convinfo', type=str, help='path to GSI convinfo file', required=True)
-parser.add_argument('-y','--yaml', type=str, help='path to output YAML file', required=True)
-parser.add_argument('-l','--loop', type=str, help='ges|anl default ges', default='ges')
+
+parser = argparse.ArgumentParser(description=('Given an input convinfo ',
+                                              'GSI file and path to ',
+                                              'GSI diags, generate an ',
+                                              'output YAML file',
+                                              'for use by PyGSI'))
+parser.add_argument('-d', '--diagdir', type=str,
+                    help='path to GSI netCDF diags', required=True)
+parser.add_argument('-c', '--cycle', type=str,
+                    help='cycle YYYYMMDDHH', required=True)
+parser.add_argument('-i', '--convinfo', type=str,
+                    help='path to GSI convinfo file', required=True)
+parser.add_argument('-y', '--yaml', type=str,
+                    help='path to output YAML file', required=True)
+parser.add_argument('-l', '--loop', type=str,
+                    help='ges|anl default ges', default='ges')
 args = parser.parse_args()
 
 config = {}
@@ -78,4 +89,3 @@ config['yaml'] = args.yaml
 config['loop'] = args.loop
 
 main(config)
-
