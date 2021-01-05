3920b799098ac0275b4ad3a92786c0a0351a4572
Cory Martin <cory.ray.martin@gmail.com>
2021-01-05 11:50:15 -0500

    Add in convinfo2yaml and satinfo2yaml scripts from DARTH to PyGSI + sample convinfo and satinfo files (#23)
    * Add convinfo2yaml and satinfo2yaml python scripts to ush/ from DARTH
    
    * Add GFSv16 convinfo and satinfo files for users; add RRFS in the future

---
+++ ush/convinfo2yaml.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# convinfo2yaml.py
+# generate YAML from input convinfo file
+# for specified input GSI diag file(s)
+import argparse
+import yaml
+import glob
+import csv
+
+
+def read_convinfo(infofile):
+    # read in convinfo file
+    obtype = []  # string used in the filename
+    typeint = []  # integer value of observation type
+    subtypeint = []  # integer value of observation subtype
+    obuse = []  # use 1, monitor -1
+    cv = open(infofile)
+    rdcv = csv.reader(filter(lambda row: row[0] != '!', cv))
+    for row in rdcv:
+        try:
+            rowsplit = row[0].split()
+            obtype.append(rowsplit[0])
+            typeint.append(int(rowsplit[1]))
+            subtypeint.append(int(rowsplit[2]))
+            obuse.append(int(rowsplit[3]))
+        except IndexError:
+            pass  # end of file
+    cv.close()
+    return obtype, typeint, subtypeint, obuse
+
+
+def main(config):
+    # call function to get lists from convinfo file
+    obtype, typeint, subtypeint, obuse = read_convinfo(config['convinfo'])
+    # get list of conventional diagnostic files available
+    diagpath = (f"{config['diagdir']}/diag_conv_*_"
+                f"{config['loop']}.{config['cycle']}.nc4")
+    diagfiles = glob.glob(diagpath)
+
+    # initialize YAML dictionary for output
+    yamlout = {'diagnostic': []}
+    if config['variable'] == 'obs':
+        diagtype = 'observation'
+    elif config['variable'] == 'hofx':
+        diagtype = 'hofx'
+    else:
+        diagtype = 'O-A' if config['loop'] == 'anl' else 'O-F'
+    figs = ['histogram', 'spatial']
+
+    # loop through obtypes
+    for iobtype, itype, isub, iuse in zip(obtype, typeint, subtypeint, obuse):
+        # first get filename and verify it exists
+        diagfile = (f"{config['diagdir']}/"
+                    f"diag_conv_{iobtype}_{config['loop']}"
+                    f".{config['cycle']}.nc4")
+        if diagfile not in diagfiles:
+            continue  # skip if diag file is missing
+        if iuse != 1:
+            continue  # only process assimilated obs for now
+        dictloop = {
+                   'path': [diagfile],
+                   'observation id': [itype],
+                   'observation subtype': [isub],
+                   'analysis use': [True],
+                   'data type': [diagtype],
+                   'plot type': figs,
+                   }
+        yamlout['diagnostic'].append({'conventional input': dictloop})
+
+    # write out the YAML
+    with open(config['yaml'], 'w') as file:
+        yaml.dump(yamlout, file, default_flow_style=False)
+    print('YAML written to '+config['yaml'])
+
+
+parser = argparse.ArgumentParser(description=('Given an input convinfo ',
+                                              'GSI file and path to ',
+                                              'GSI diags, generate an ',
+                                              'output YAML file',
+                                              'for use by PyGSI'))
+parser.add_argument('-d', '--diagdir', type=str,
+                    help='path to GSI netCDF diags', required=True)
+parser.add_argument('-c', '--cycle', type=str,
+                    help='cycle YYYYMMDDHH', required=True)
+parser.add_argument('-i', '--convinfo', type=str,
+                    help='path to GSI convinfo file', required=True)
+parser.add_argument('-y', '--yaml', type=str,
+                    help='path to output YAML file', required=True)
+parser.add_argument('-l', '--loop', type=str,
+                    help='ges|anl default ges', default='ges')
+parser.add_argument('-v', '--variable', type=str,
+                    help='read departures, obs, or H(x): omf | obs | hofx',
+                    default='omf')
+args = parser.parse_args()
+
+config = {}
+config['diagdir'] = args.diagdir.rstrip('/')
+config['cycle'] = args.cycle
+config['convinfo'] = args.convinfo
+config['yaml'] = args.yaml
+config['loop'] = args.loop
+config['variable'] = args.variable
+
+main(config)
---
+++ ush/satinfo2yaml.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# satinfo2yaml.py
+# generate YAML from input satinfo file
+# for specified input GSI diag file(s)
+import argparse
+import yaml
+import glob
+import csv
+
+
+def read_satinfo(infofile):
+    # read in satinfo file
+    sensor = []  # string used in the filename
+    channel = []  # integer value of channel
+    obuse = []  # use 1, monitor -1
+    cv = open(infofile)
+    rdcv = csv.reader(filter(lambda row: row[0] != '!', cv))
+    for row in rdcv:
+        try:
+            rowsplit = row[0].split()
+            sensor.append(rowsplit[0])
+            channel.append(int(rowsplit[1]))
+            obuse.append(int(rowsplit[2]))
+        except IndexError:
+            pass  # end of file
+    cv.close()
+    return sensor, channel, obuse
+
+
+def main(config):
+    # call function to get lists from satinfo file
+    sensor, channel, obuse = read_satinfo(config['satinfo'])
+    # get list of diagnostic files available
+    diagpath = (f"{config['diagdir']}/diag_*_"
+                f"{config['loop']}.{config['cycle']}.nc4")
+    diagfiles = glob.glob(diagpath)
+
+    # initialize YAML dictionary for output
+    yamlout = {'diagnostic': []}
+    if config['variable'] == 'obs':
+        diagtype = 'observation'
+    elif config['variable'] == 'hofx':
+        diagtype = 'hofx'
+    else:
+        diagtype = 'O-A' if config['loop'] == 'anl' else 'O-F'
+    figs = ['histogram', 'spatial']
+
+    # loop through obtypes
+    for isensor, iuse, ichan in zip(sensor, obuse, channel):
+        # first get filename and verify it exists
+        diagfile = (f"{config['diagdir']}/"
+                    f"diag_{isensor}_{config['loop']}"
+                    f".{config['cycle']}.nc4")
+        if diagfile not in diagfiles:
+            continue  # skip if diag file is missing
+        if iuse != 1:
+            continue  # only process assimilated obs for now
+        dictloop = {
+                   'path': [diagfile],
+                   'channel': [ichan],
+                   'qc flag': [0],
+                   'data type': [diagtype],
+                   'plot type': figs,
+                   }
+        yamlout['diagnostic'].append({'radiance input': dictloop})
+
+    # write out the YAML
+    with open(config['yaml'], 'w') as file:
+        yaml.dump(yamlout, file, default_flow_style=False)
+    print('YAML written to '+config['yaml'])
+
+
+parser = argparse.ArgumentParser(description=('Given an input satinfo ',
+                                              'GSI file and path to ',
+                                              'GSI diags, generate an output ',
+                                              'YAML file for use by PyGSI'))
+parser.add_argument('-d', '--diagdir', type=str,
+                    help='path to GSI netCDF diags', required=True)
+parser.add_argument('-c', '--cycle', type=str,
+                    help='cycle YYYYMMDDHH', required=True)
+parser.add_argument('-i', '--satinfo', type=str,
+                    help='path to GSI satinfo file', required=True)
+parser.add_argument('-y', '--yaml', type=str,
+                    help='path to output YAML file', required=True)
+parser.add_argument('-l', '--loop', type=str,
+                    help='ges|anl default ges', default='ges')
+parser.add_argument('-v', '--variable', type=str,
+                    help='read departures, obs, or H(x): omf | obs | hofx',
+                    default='omf')
+args = parser.parse_args()
+
+config = {}
+config['diagdir'] = args.diagdir.rstrip('/')
+config['cycle'] = args.cycle
+config['satinfo'] = args.satinfo
+config['yaml'] = args.yaml
+config['loop'] = args.loop
+config['variable'] = args.variable
+
+main(config)
