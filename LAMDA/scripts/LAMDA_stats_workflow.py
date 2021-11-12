aee56ac860f7b7813c6fec29920112be342eb45e
Kevin Dougherty <kevin.dougherty@noaa.gov>
2021-11-12 17:41:41 +0000

    stats workflow updated

--- LAMDA/scripts/LAMDA_stats_workflow.py
+++ LAMDA/scripts/LAMDA_stats_workflow.py
@@ -1,3 +1,4 @@
+import yaml
 import glob
 import os
 import pandas as pd
@@ -7,18 +8,35 @@ from multiprocessing import Pool
 from functools import partial
 from datetime import datetime
 from pyGSI.gsi_stat import GSIstat
-# Add imports of specific plotting scripts
+
+# import the plotting scripts i.e:
+# from LAMDA.obs_count import plot_obscount
+# from LAMDA.minimization import plot_minimization
 
 
-def concatenate_dfs(files, variable, cycles):
+def concatenate_dfs(files, variable, cycles, data_type):
     """
     Reads in list of files and creates a concatenated
     dataframe of all cycles.
+
+    Args:
+        files : (list) list of files
+        variable : (str) data obs type i.e. t, uv, q etc.
+        cycles : (list) list of str cycles
+        data_type : (str) the input data type i.e conventional
+                    or radiance
+    Returns:
+        concatenated_df : concatenated dataframe over cycles
     """
     dfs = []
     for i, file in enumerate(files):
-        gdas = GSIstat(file, cycles[i])
-        df = gdas.extract(variable)
+        gstat = GSIstat(file, cycles[i])
+
+        if data_type == 'radiance':
+            df = gstat.extract_instrument('rad', variable)
+        else:
+            df = gstat.extract(variable)
+
         dfs.append(df)
 
     concatenated_df = pd.concat(dfs)
@@ -26,40 +44,90 @@ def concatenate_dfs(files, variable, cycles):
     return concatenated_df
 
 
-def plotting_func(config, data_dict, outdir):
+def plotting(config, data_dict, outdir, data_type, ob_type):
+    """
+    Main plotting function that gets all inputs for plotting scripts
+    in order which includes fits_df, plotting_config, and outdir.
 
-    variable = config[0]
+    Args:
+        config : (list of tuples) the multiprocessing inputs
+                 including the plot type to create and the data
+                 inputs (either conventional (obsids, subtype, obuse)
+                 or radiance (channel, obuse))
+        data_dir : (dict) dictionary that includes fits data
+        outdir : (str) path to where figures should be outputted
+        data_type : (str) determines if data is conventional or
+                    radiance
+        ob_type : (str) the observation type
+    """
+
+    data_inputs = config[0]
     plot_type = config[-1]
 
-    for tm in np.arange(6):
+    plotting_config = {}
+    plotting_config['ob_type'] = ob_type
+
+    if data_type == 'conventional':
+        plotting_config['obsid'] = data_inputs[0]
+        plotting_config['subtype'] = data_inputs[1]
+        plotting_config['obuse'] = data_inputs[-1]
+
+    elif data_type == 'radiance':
+        # change ob_type to just the sensor to utilize
+        # GSIstat extract_sensor()
+        ob_type = ob_type.split('_')[0]
+        plotting_config['channel'] = data_inputs[0]
+        plotting_config['obuse'] = data_inputs[-1]
+
+    # Loop through t-minus hours to grab proper data and
+    # generate plots
+    for tm in np.arange(7):
         fits_data = []
-        fits2_data = []
         cycles = []
 
         for cycle in data_dict.keys():
             fits_data.append(data_dict[cycle]['fits'][tm])
+            cycles.append(cycle)
+
+        # Concatenate all files into one dataframe
+        fits_df = concatenate_dfs(fits_data, ob_type, cycles, data_type)
+
+        plot_dict = {
+            'obs count': plot_obscount,
+        }
+
+        plot_dict[plot_type](fits_df, plotting_config, outdir)
+
+
+def create_minimization_plots(data_dict, outdir):
+    """
+    Since it is stand alone from other plotting scripts, this
+    functions purpose is to generate the minimization plots.
+
+    Args:
+        data_dir : (dict) dictionary that includes fits2 data
+        outdir : (str) path to where figures should be outputted
+    """
+
+    # Loop through t-minus hours to grab proper data and
+    # generate plots
+    for tm in np.arange(7):
+        fits2_data = []
+        cycles = []
+
+        for cycle in data_dict.keys():
             fits2_data.append(data_dict[cycle]['fits2'][tm])
             cycles.append(cycle)
 
-        if plot_type == 'minimization':
-            # Minimization plots
-            fits2_df = concatenate_dfs(fits2_data, variable, cycles)
-            minimization_plots(fits2_df, outdir)
+        # Concatenate all files into one dataframe
+        fits2_df = concatenate_dfs(fits_data, 'cost', cycles,
+                                   data_type='cost')
 
-        else:
-            # Other stats plots
-            fits_df = concatenate_dfs(fits_data, var, cycles)
-
-            plot_dict = {
-                'obs count': obs_count,
-                'rmse bias': rmse_bias,
-                'error standard dev': error_std_dev
-            }
-
-            plot_dict[plot_type](fits_df, outdir)
+        # Create plot by calling plotting script
+        minimization_plots(fits2_df, outdir)
 
 
-def stats_workflow(config_yaml, nprocs):
+def stats_workflow(config_yaml, nprocs, outdir):
     """
     Main function that reads input yaml and sorts
     GSI stat data correctly.
@@ -70,22 +138,26 @@ def stats_workflow(config_yaml, nprocs):
                       plot types, and out directory
         nprocs : (int) number of processors to use for
                  multiprocessing
+        outdir : (str) path to output diagnostics
     """
 
-    inpath = config_yaml['inpath']
-    outdir = config_yaml['outdir']
-    variables = config_yaml['variables']
+    # Open config yaml files
+    with open(config_yaml, 'r') as file:
+        config_yaml = yaml.load(file, Loader=yaml.FullLoader)
+
+    statdir = config_yaml['stat']['stat dir']
+    data_type = config_yaml['stat']['data type']
+    ob_type = config_yaml['stat']['ob type']
     plot_types = config_yaml['plot types']
 
     # Grabs all subdirectories of stats files (in date subdirectory)
-    subdirs = sorted([f.path for f in os.scandir(inpath) if f.is_dir()])
+    subdirs = sorted([f.path for f in os.scandir(statdir) if f.is_dir()])
 
     # Create dictionary of all stats files sorted by date
     data_dict = {}
 
     for subdir in subdirs:
         cycle = subdir.split('/')[-1]
-
         fits = glob.glob(subdir + '/*fits.*')
         fits2 = glob.glob(subdir + '/*fits2.*')
 
@@ -93,30 +165,25 @@ def stats_workflow(config_yaml, nprocs):
         data_dict[cycle]['fits'] = sorted(fits)
         data_dict[cycle]['fits2'] = sorted(fits2)
 
-    # Create work list
+    # minimization plots are on their own so need to separate
+    # remove from plot list and then call function to plot
     if 'minimization' in plot_types:
         plot_types.remove('minimization')
-        work_list = list(itertools.product(variables, plot_types))
-        work_list.append(('cost', 'minimization'))
+        create_minimization_plots(data_dict, outdir)
 
-    else:
-        work_list = list(itertools.product(variables, plot_types))
+    # Create multiprocessing lists
+    if data_type == 'conventional':
+        conv_inputs = list(zip(config_yaml['stat']['observation id'],
+                               config_yaml['stat']['observation subtype'],
+                               config_yaml['stat']['obuse']))
+        work_list = list(itertools.product(conv_inputs, plot_types))
+
+    elif data_type == 'radiance':
+        sat_inputs = list(zip(config_yaml['stat']['channels'],
+                              config_yaml['stat']['obuse']))
+        work_list = list(itertools.product(sat_inputs, plot_types))
 
     # Create multiprocessing Pool
     p = Pool(processes=nprocs)
-    p.map(partial(plotting_func, data_dict=data_dict,
-                  outdir=outdir), work)
-
-
-if __name__ == '__main__':
-    # Parse command line
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-n", "--nprocs",
-                    help="Number of tasks/processors for multiprocessing")
-    ap.add_argument("-y", "--yaml_input",
-                    help="Path to yaml file")
-
-    myargs = ap.parse_args()
-    nprocs = int(myargs.nprocs) if myargs.nprocs else 1
-
-    stats_workflow(myargs.input_yaml, nprocs)
+    p.map(partial(plotting, data_dict=data_dict, outdir=outdir,
+                  ob_type=ob_type, data_type=data_type), work_list)
