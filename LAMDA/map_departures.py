eec08dfded9b3f3467eac97c385c51fb78fb43ae
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2021-11-17 20:58:52 +0000

    Working OmF map

--- LAMDA/map_departures.py
+++ LAMDA/map_departures.py
@@ -21,7 +21,7 @@ def _create_map_departures(df, domain, projection,
     # grab variables for plotting
     lats = df['latitude'].to_numpy()
     lons = df['longitude'].to_numpy()
-    omf = df['hofx_adjusted'].to_numpy()
+    omf = df['omf_adjusted'].to_numpy()
 
     plot_objects = []
 
@@ -39,12 +39,33 @@ def _create_map_departures(df, domain, projection,
                                 data=omf)
         omfscatter.markersize = 1
         omfscatter.cmap = 'coolwarm'
+        # determine min/max of values
+        maxval = np.nanmax(omf)
+        minval = np.nanmin(omf)
+        vmax = max(abs(minval), maxval)
+        omfscatter.vmin = vmax * -1
+        omfscatter.vmax = vmax
         # add data to plot objects
         plot_objects.append(omfscatter)
         # labels and annotations
         labels = features.get_labels(metadata)
         # draw data
         mymap.draw_data(plot_objects)
+        # add labels
+        mymap.add_colorbar(label=metadata['Diag Type'],
+                           label_fontsize=12, extend='neither')
+        mymap.add_title(labels['title'], loc='left', fontsize=12)
+        mymap.add_title(labels['date title'], loc='right', fontsize=12,
+                        fontweight='semibold')
+        mymap.add_xlabel("Longitude", fontsize=12)
+        mymap.add_ylabel("Latitude", fontsize=12)
+        # add stats to figure
+        stats_dict = {
+            'nobs': len(lats),
+            'min': minval,
+            'max': maxval,
+        }
+        mymap.add_stats_dict(stats_dict=stats_dict)
         # save figure
         fig = mymap.return_figure()
         str_domain = domain.replace(" ", "_")
@@ -73,25 +94,27 @@ def map_departures(config):
 
     if filetype == 'conv':
         diag = Conventional(config['diag file'])
-
         df = diag.get_data(obsid=[config['observation id']],
                            subtype=[config['observation subtype']],
                            analysis_use=config['analysis use'])
         metadata = diag.metadata
-        metadata['ObsID Name'] = features.get_obs_type(
-            [config['observation id']])
+        metadata['ObsID Name'] = features.get_obs_type([config['observation id']])
 
     else:
         diag = Radiance(config['diag file'])
-
         df = diag.get_data(channel=[config['channel']],
-                           qcflag=[config['qc flag']],
                            analysis_use=config['analysis use'])
         metadata = diag.metadata
 
     metadata['Diag Type'] = config['data type']
-    metadata['Anl Use Type'] = None
 
-    # pass dataframe to plot function
-    _create_map_departures(df['assimilated'], config['domain'], config['projection'],
-                           metadata, config['outdir'])
+    anl_use = config['analysis use']
+    if anl_use:
+        for anl_type in df.keys():
+            metadata['Anl Use Type'] = anl_type
+            _create_map_departures(df[anl_type], config['domain'], config['projection'],
+                                   metadata, config['outdir'])
+    else:
+        metadata['Anl Use Type'] = None
+        _create_map_departures(df, config['domain'], config['projection'],
+                               metadata, config['outdir'])
