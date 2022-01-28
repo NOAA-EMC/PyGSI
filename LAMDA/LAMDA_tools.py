fc775e41085af5270ba6d5a7047b787d518b66de
Kevin Dougherty <kevin.dougherty@noaa.gov>
2022-01-28 18:43:05 +0000

    add LAMDA filtering tools

---
+++ LAMDA/LAMDA_tools.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+
+
+class LAMDA_Tools:
+
+    def __init__(self):
+        return
+
+    def land_fraction(self, df):
+        """
+        Returns dataframe for only land data.
+        """
+        df = df[df['land_fraction'] > 0]
+
+        return df
+
+    def water_fraction(self, df):
+        """
+        Returns dataframe for only water data.
+        """
+        df = df[df['water_fraction'] > 0]
+
+        return df
+
+    def cloud_fraction(self, df):
+        """
+        Returns dataframe for only cloud data.
+        """
+        df = df[df['cloud_fraction'] > 0]
+
+        return df
+
+    def vegetation_fraction(self, df):
+        """
+        Returns dataframe for only vegetation data.
+        """
+        df = df[df['vegetation_fraction'] > 0]
+
+        return df
+
+    def ice_fraction(self, df):
+        """
+        Returns dataframe for only ice data.
+        """
+        df = df[df['ice_fraction'] > 0]
+
+        return df
+
+    def snow_fraction(self, df):
+        """
+        Returns dataframe for only snow data.
+        """
+        df = df[df['snow_fraction'] > 0]
+
+        return df
