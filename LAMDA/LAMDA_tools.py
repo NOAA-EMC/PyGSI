f33b1d7261db8aeb720bae24d768a46095f8217f
Kevin Dougherty <kevin.dougherty@noaa.gov>
2022-01-28 18:49:26 +0000

    changed to functions

--- LAMDA/LAMDA_tools.py
+++ LAMDA/LAMDA_tools.py
@@ -2,55 +2,50 @@ import numpy as np
 import pandas as pd
 
 
-class LAMDA_Tools:
+def land_fraction(self, df):
+    """
+    Returns dataframe for only land data.
+    """
+    df = df[df['land_fraction'] > 0]
 
-    def __init__(self):
-        return
+    return df
 
-    def land_fraction(self, df):
-        """
-        Returns dataframe for only land data.
-        """
-        df = df[df['land_fraction'] > 0]
+def water_fraction(self, df):
+    """
+    Returns dataframe for only water data.
+    """
+    df = df[df['water_fraction'] > 0]
 
-        return df
+    return df
 
-    def water_fraction(self, df):
-        """
-        Returns dataframe for only water data.
-        """
-        df = df[df['water_fraction'] > 0]
+def cloud_fraction(self, df):
+    """
+    Returns dataframe for only cloud data.
+    """
+    df = df[df['cloud_fraction'] > 0]
 
-        return df
+    return df
 
-    def cloud_fraction(self, df):
-        """
-        Returns dataframe for only cloud data.
-        """
-        df = df[df['cloud_fraction'] > 0]
+def vegetation_fraction(self, df):
+    """
+    Returns dataframe for only vegetation data.
+    """
+    df = df[df['vegetation_fraction'] > 0]
 
-        return df
+    return df
 
-    def vegetation_fraction(self, df):
-        """
-        Returns dataframe for only vegetation data.
-        """
-        df = df[df['vegetation_fraction'] > 0]
+def ice_fraction(self, df):
+    """
+    Returns dataframe for only ice data.
+    """
+    df = df[df['ice_fraction'] > 0]
 
-        return df
+    return df
 
-    def ice_fraction(self, df):
-        """
-        Returns dataframe for only ice data.
-        """
-        df = df[df['ice_fraction'] > 0]
+def snow_fraction(self, df):
+    """
+    Returns dataframe for only snow data.
+    """
+    df = df[df['snow_fraction'] > 0]
 
-        return df
-
-    def snow_fraction(self, df):
-        """
-        Returns dataframe for only snow data.
-        """
-        df = df[df['snow_fraction'] > 0]
-
-        return df
+    return df
