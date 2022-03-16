f7287579e000a1e1072fe72e1a0bf3848af3f82e
Andrew Collard <40322596+ADCollard@users.noreply.github.com>
2022-03-16 12:15:02 -0400

    Add spectrum statistics capability (#117)
    * Add ability to read radiance Jacobians
    
    * Update diags.py
    
    Fixed pycodestyle issues
    
    * Update diags.py
    
    2nd attempt to fit pycodestyle
    
    * Add optional inputs and ug fix
    
    * Update diags.py
    
    pycodesyle fixes
    
    * Update diags.py
    
    pycodestyle
    
    * Update diags.py
    
    * Update diags.py
    
    * Update diags.py
    
    * Update diags.py
    
    Trying again
    
    * GitHub Issue NOAA-EMC/PyGSI#116: Add spectral stats capability

---
+++ pyGSI/stats.py
@@ -0,0 +1,49 @@
+# coding: utf-8 -*-
+
+'''
+stats.py contains statistics utility functions
+'''
+
+__all__ = ['spectrum_stats']
+
+import pandas as _pd
+
+
+def spectrum_stats(df):
+
+    '''
+    Function to calculate major statistics for radiance data as a function of channel.
+    Statisics are for observations that pass QC only.
+
+    Args:
+        df : (Pandas dataframe) A data frame containing raw radiance information as produced
+             by the get_data method of Radiance.
+
+    Returns:
+        channel_stats : (Pandas dataframe) A data frame containing channel-by-channel statistics
+    '''
+
+    # Get channel numbers in dataframe
+    sc = df.index.unique(level="Channel")
+
+    # Initialise output dataframe
+    channel_stats = _pd.DataFrame(index=sc,
+                                  columns=["count", "omf_unadjusted_mean", "omf_adjusted_mean",
+                                           "omf_unadjusted_stddev", "omf_adjusted_stddev"])
+
+    # Sorting by channel increases efficiency and prevents a warning message
+    dfs = df.sort_index()
+
+    # Loop through channels and calculate stats
+    for chan in sc:
+        # Subset based on channel and QC indexes
+        tmp = dfs.loc[(chan, 0.0)]
+        # This next line makes sure that all rejected obs are avoided.
+        # Using the inverse observation error is more reliable than the QC flag.
+        tmp = tmp[(tmp['inverse_observation_error'] > 0.0)]
+        channel_stats["count"].loc[chan] = tmp.size
+        for var in ["omf_unadjusted", "omf_adjusted"]:
+            channel_stats[var + '_mean'].loc[chan] = tmp[var].to_numpy().mean()
+            channel_stats[var + '_stddev'].loc[chan] = tmp[var].to_numpy().std()
+
+    return channel_stats
